8696124c9a2c57828be7a1c83b6b97d368884c7e
CapstoneIDCC25CF292 <itspiroy@gmail.com>
2025-05-27 20:03:03 +0700

    Update loadModel.js

--- utils/loadModel.js
+++ utils/loadModel.js
@@ -6,8 +6,8 @@ let model = null;
 async function getModel() {
   if (!model) {
     model = await tf.loadGraphModel(
-      // "https://plantcare.up.railway.app/model/model.json"
-      "http://localhost:3001/model/model.json"
+      "https://plantcare.up.railway.app/model/model.json"
+      // "http://localhost:3001/model/model.json"
     );
     console.log("✅ Model loaded");
   }
